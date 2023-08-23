1189aa7338bb2c91f3d4951bb38d7bfd3f970779
x.ande <ananfro@live.com>
2023-08-23 13:52:19 +1000

    handlebars set up on server

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require("express");
-const sequelize = require("./db/connection.js");
+const exphbs = require("express-handlebars");
+const sequelize = require("./db/connection.js"); //SQL connection
 const path = require("path");
-require("./models");
+require("./models"); // Import models
 
+// SETUP express
 const server = express();
 const PORT = process.env.PORT || 3001;
 
+// SETUP express-handlebars
+const hbs = exphbs.create({});
+server.engine("handlebars", hbs.engine);
+server.set("view engine", "handlebars");
+
 server.use(express.json());
 server.use(express.urlencoded({ extended: true }));
-// server.use(express.static(path.join(__dirname, "public")));
+server.use(express.static(path.join(__dirname, "public")));
 
 server.use(require("./routes"));
 
